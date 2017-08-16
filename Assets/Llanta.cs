122707d47b47aab9ed60c462c7bf8a50f6202cdc
Augusto <augustosalazar@uninorte.edu.co>
2017-08-16 17:23:25 -0500

    first commit

---
+++ Assets/Llanta.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Llanta : MonoBehaviour {
+    public WheelCollider collider;
+	// Use this for initialization
+	void Start () {
+        collider = GetComponentInParent<WheelCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ Assets/Motor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour {
+    public WheelCollider LDI, LDD, LTI, LTD;
+    public float FuerzaDeMotor;
+    public float chancleta;
+    public float cabrilla;
+    public float frenoDeMano;
+    public float rotacionMaximaDeLlantas;
+    public float FuerzaDeFrenoDeMano;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        chancleta = Input.GetAxis("Vertical");
+        cabrilla = Input.GetAxis("Horizontal");
+        frenoDeMano = Input.GetAxisRaw("Jump");
+        LDI.motorTorque = chancleta * FuerzaDeMotor * Time.deltaTime;
+        LDD.motorTorque = chancleta * FuerzaDeMotor * Time.deltaTime;
+        LDD.steerAngle = cabrilla * rotacionMaximaDeLlantas;
+        LDI.steerAngle = cabrilla * rotacionMaximaDeLlantas;
+        
+        if (frenoDeMano > 0f)
+        {
+            LTI.brakeTorque = FuerzaDeFrenoDeMano;
+            LTD.brakeTorque = FuerzaDeFrenoDeMano;
+        }
+        else {
+            LTI.brakeTorque = 0f;
+            LTD.brakeTorque = 0f;
+        }
+        
+    }
+}
